d04040c7b1145f4c6eb142f745314e4222ca8afd
Os <GizatulinsO@Gmail.com>
2020-06-24 20:26:12 +0300

    Before work commit

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,43 +1,93 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import Typography from "@material-ui/core/Typography";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   list: {
     width: 250,
   },
   fullList: {
     width: "auto",
   },
-});
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
 
 export default ({ open, toggleDrawer }) => {
+  const [selectedIndex, setSelectedIndex] = React.useState(null);
+  const [listOpen, setListOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setListOpen(!listOpen);
+  };
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
   const classes = useStyles();
 
   const sideList = (side) => (
     <div
       className={classes.list}
       role="presentation"
-      onClick={toggleDrawer(side, false)}
+      //onClick={toggleDrawer(side, false)}
       onKeyDown={toggleDrawer(side, false)}
     >
       <List>
-        {["News", "Documentation", "Planner", "Forum"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItem
+          button
+          selected={selectedIndex === 0}
+          onClick={(event) => handleListItemClick(event, 0)}
+        >
+          <ListItemText primary="News" />
+        </ListItem>
+        <ListItem
+          button
+          selected={selectedIndex === 1}
+          onClick={(event) => handleListItemClick(event, 1)}
+        >
+          <ListItemText primary="Documentation" />
+          {listOpen ? (
+            <ExpandLess onClick={(e) => handleClick()} />
+          ) : (
+            <ExpandMore onClick={(e) => handleClick()} />
+          )}
+        </ListItem>
+        <Collapse in={listOpen} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="CHAPTER 1" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="CHAPTER 2" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="CHAPTER 3" />
+            </ListItem>
+          </List>
+        </Collapse>
+        <ListItem
+          button
+          selected={selectedIndex === 2}
+          onClick={(event) => handleListItemClick(event, 2)}
+        >
+          <ListItemText primary="Planner" />
+        </ListItem>
+        <ListItem
+          button
+          selected={selectedIndex === 3}
+          onClick={(event) => handleListItemClick(event, 3)}
+        >
+          <ListItemText primary="Forum" />
+        </ListItem>
       </List>
     </div>
   );
